280ed721af25d42e1d389fd113b9ae4f521f31a1
artem.stolyarenko <roguenalan@gmail.com>
2018-06-30 15:38:31 +0300

    Added Windows Forms app

---
+++ UfrawParallel/UfrawParallel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UfrawParallel
+{
+	public partial class UfrawParallel : Form
+	{
+		public UfrawParallel()
+		{
+			InitializeComponent();
+		}
+	}
+}
