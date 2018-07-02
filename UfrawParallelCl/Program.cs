f6c997385c207e9fb220831e8839995ba08b29c1
artem.stolyarenko <roguenalan@gmail.com>
2018-07-02 09:40:22 +0300

    * .NET Core console project added * Reference to UFRawParallel.Core added to project

---
+++ UfrawParallelCl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UfrawParallelCl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
